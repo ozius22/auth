0ea6ee5c5d9affca2d3737e3c289a42d578ef109
Luis <lsuizo72@gmail.com>
2024-02-29 23:49:51 +0800

    initial

---
+++ client/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="center">
+      <button @click="goTo('/login')">Log In</button>
+      <button class="but2" @click="goTo('/register')">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goTo(path) {
+      this.$router.push(path);
+    }
+  }
+}
+</script>
+
+<style>
+.center {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; 
+}
+
+.but2 {
+  margin-right: 10px;
+}
+</style>
